fb421f53a935e9d1d9a04bef60932986053bd66c
sargam5 <kushwahasargam987@gmail.com>
2026-06-18 11:15:25 +0530

    maine data type and operator padha

---
+++ fd10/operator.js
@@ -0,0 +1,19 @@
+// let a=9
+// b=3
+// c=10
+// console.log(a+b+c);
+// console.log( a-b-c);
+// let d=a*b
+// console.log(d);
+// let e=a/b
+// console.log(e);
+// f=a%c
+// console.log(f);
+// g=a**b
+// console.log(g);
+a=1
+a++
+console.log(a);
+
+console.log(a++);
+console.log(a);

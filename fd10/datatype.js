a39c7a294c9e228f015036edf546d4e322e3fa3c
sargam5 <kushwahasargam987@gmail.com>
2026-06-18 10:16:58 +0530

    first commit

---
+++ fd10/datatype.js
@@ -0,0 +1,28 @@
+// let name="sargam"
+// console.log(name);
+// console.log(typeof(name));
+// // number
+// let num=1
+// console.log(num);
+// console.log(typeof(num));
+
+//  let b=false
+// console.log(b);
+// console.log(typeof(b));
+// // undefined let c
+//  let c
+//  console.log(c);
+//  console.log(typeof(c));
+// // null
+//  
+let d=null
+console.log(d);
+console.log(typeof(d));
+// symbol
+let e=Symbol("sargam")
+console.log(e);
+console.log(typeof(e));
+
+let f=BigInt("1234567890123456789012345678901234567890")
+console.log(f);
+console.log(typeof(f));

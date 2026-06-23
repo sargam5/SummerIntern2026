20efdabbc550c2b072451aea8120ba26320777eb
sargam5 <kushwahasargam987@gmail.com>
2026-06-23 18:55:42 +0530

    hw is here

---
+++ fd14/hw.js
@@ -0,0 +1,30 @@
+let fruits=[ "mango","apple","papaya","banana","grapes"]
+
+console.log(fruits)
+
+ p=fruits.pop()
+ console.log(p);
+
+ q=fruits.shift()
+ console.log(q);
+
+ fruits.push("pineapple")
+ console.log(fruits);
+
+ fruits.pop()
+ console.log(fruits);
+
+ let num=[1,4,6,8,9,]
+ add=num[1]+num[3]
+ console.log(add);
+sum=0;
+ for(i=0;i<=4;i++){
+    sum=sum+num[i]
+ }
+ console.log("The sum of all is:",sum);
+ 
+
+ for (i=0;i<=4;i++){
+    console.log(num[i]);
+    
+ }
